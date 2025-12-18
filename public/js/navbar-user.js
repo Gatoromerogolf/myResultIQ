b8b88f5642c46e8d61e1ba30d49dad4184e80470
Gatoromerogolf <114531023+Gatoromerogolf@users.noreply.github.com>
2025-12-18 16:58:29 -0300

    ultima actualizacion

--- public/js/navbar-user.js
+++ public/js/navbar-user.js
@@ -13,18 +13,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const userNameEl = document.getElementById('userName');
     const userAvatarEl = document.getElementById('userAvatar');
+    const userAvatarDrop = document.getElementById('userAvatarDropdown');
+    const userNameDrop = document.getElementById('userNameDropdown');
+    const userRolDrop = document.getElementById('userRole');
+
 
     if (userNameEl) {
         userNameEl.innerHTML = `
             <strong>${usuario.nombres} ${usuario.apellido}</strong>
             <span class="text-muted">(${usuario.rol.toUpperCase()})</span>
         `;
+
+        userNameDrop.innerHTML = userNameEl.innerHTML;
+        userRolDrop.innerHTML = `${usuario.rol}`;
     }
 
     userAvatarEl.src = `/usuarios/foto/${usuario.legajo}?t=${Date.now()}`;
+    userAvatarDrop.src = `/usuarios/foto/${usuario.legajo}?t=${Date.now()}`;
 
     userAvatarEl.onerror = () => {
         userAvatarEl.src = '/dist/images/default_user.jpg';
+        userAvatarDrop.src = '/dist/images/default_user.jpg';
     };
 
 });
